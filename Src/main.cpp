d192b734731d2a2cf78e6dc507de3113f93c7446
Dinesh Mali DKm <dkmsn8@gmail.com>
2018-04-22 10:39:32 +0530

    init

---
+++ Src/main.cpp
@@ -0,0 +1,15 @@
+
+
+
+
+#include <QApplication>
+
+#include "calculator.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Calculator calc;
+    calc.show();
+    return app.exec();
+}
